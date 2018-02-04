984794feba872ba85af391d63aee2c26aae85669
vaishnavisuguru <vsuguru@unomaha.edu>
2018-02-04 17:41:52 -0600

    Additional files and changes for Heroku

---
+++ polls/urls.py
@@ -0,0 +1,14 @@
+from . import views
+from django.conf.urls import url
+
+app_name = 'polls'
+urlpatterns = [
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    # ex: /polls/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+]
+
